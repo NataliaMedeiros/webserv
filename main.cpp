3c8e3153d90bcaed1a18cc5985b4453efd7483a6
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2026-03-20 11:30:47 +0100

    added main for tests

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "HttpRequest.hpp"
+
+int main()
+{
+	std::string IncompleteRawRequest =
+		"POST /login HTTP/1.1\r\n"
+		"Host: example.com\r\n"
+		"Content-Length: 12\r\n"
+		""
+		"username=joe";
+
+    std::string rawRequest =
+        "POST /login HTTP/1.1\r\n"
+        "Host: example.com\r\n"
+        "Content-Length: 12\r\n"
+        "\r\n"
+        "username=joe";
+
+	std::cout << "=== Testing incomplete request===\n";
+	findHeaderEnd(IncompleteRawRequest);
+	std::cout << "=== Find headers end===\n";
+	findHeaderEnd(rawRequest);
+    // std::string headerPart, bodyPart;
+    // splitHeaderBody(rawRequest, headerPart, bodyPart);
+
+    // std::cout << "\n=== Step 3: Split headers into lines ===\n";
+    // std::vector<std::string> lines = splitLines(headerPart);
+
+    // std::cout << "\n=== Step 4: Parse start line ===\n";
+    // HttpRequest request;
+    // parseStartLine(request, lines[0]);
+
+    // std::cout << "\n=== Step 5: Parse headers ===\n";
+    // parseHeaders(request, lines);
+
+    // std::cout << "\n=== Step 6: Parse body ===\n";
+    // parseBody(request, bodyPart);
+
+    // std::cout << "\n=== Final HttpRequest object ===\n";
+    // std::cout << "Method: " << request.method << "\n";
+    // std::cout << "Path: " << request.path << "\n";
+    // std::cout << "Version: " << request.version << "\n";
+    // for (auto& h : request.headers)
+    //     std::cout << h.first << ": " << h.second << "\n";
+    // std::cout << "Body: " << request.body << "\n";
+
+    return 0;
+}
